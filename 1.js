e9af0fad146ae5cb088d3cd2a8932c143d440385
Xartok <lagache.yoann@gmail.com>
2017-10-20 23:19:33 +0200

    Generalize solution of problem 1

--- 1.js
+++ 1.js
@@ -1,16 +1,20 @@
 /**
- * Multiples of 3 and 5
+ * Find the sum of all the multiples of 3 or 5 below 1000
  */
-Array(999).join().split(",")
-// range 1-999
-.map(function(v, i) {
-    return i + 1;
-})
-// multiple of 3 or 5
-.filter(function(v) {
-    return v % 3 === 0 || v % 5 === 0;
-})
-// sum
-.reduce(function(a, b) {
-    return a + b;
-}, 0);
+function multiplesOfThreeAndFiveBelowN(n) {
+	return Array(n-1).join().split(",")
+	// range 1-n-1
+	.map(function(v, i) {
+	    return i + 1;
+	})
+	// multiple of 3 or 5
+	.filter(function(v) {
+	    return v % 3 === 0 || v % 5 === 0;
+	})
+	// sum
+	.reduce(function(a, b) {
+	    return a + b;
+	}, 0);
+}
+
+console.log(multiplesOfThreeAndFiveBelowN(1000));
